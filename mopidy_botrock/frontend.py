6ece9eed63b5a18bf8eda802190a1de2d8a0767d
Brad Havens <bhavens@hawk.iit.edu>
2017-12-08 08:15:33 +0000

    Added 'playNow' functionality

--- mopidy_botrock/frontend.py
+++ mopidy_botrock/frontend.py
@@ -28,8 +28,8 @@ class BotRockFrontend(pykka.ThreadingActor, core.CoreListener):
 		self.topic = self.config['topic']
 
 		self.client = mqtt.Client()
-		self.client.on_connect = self.on_connect
-		self.client.on_message = self.on_message
+		self.client.on_connect = self.on_mqtt_connect
+		self.client.on_message = self.on_mqtt_message
 
 		#if _secretkey:
 		#	self.client.username_pw_set(_secretkey)
@@ -41,22 +41,27 @@ class BotRockFrontend(pykka.ThreadingActor, core.CoreListener):
 		self.client.loop_start()
 		super(BotRockFrontend, self).__init__()
 
-	def on_connect(self, client, data, flags, rc):
+	def on_mqtt_connect(self, client, data, flags, rc):
 		client.subscribe(self.topic, 1)
 		logger.info("Connected to topic: " + self.topic)
-	def on_message(self, client, data, msg):
+	
+	def on_mqtt_message(self, client, data, msg):
 		logger.info("Received a message on " + msg.topic + " with payload " + str(msg.payload))
 		payload = json.loads(msg.payload)
 		#logger.info(payload)
 		data = payload[u'data']
 		action = data[u'action']
 		self.handle_action(action, data)
+
 	def handle_action(self, action, data = None):
-		logger.info('handle_action')
 		try:
 			logger.info("Action: " + action)
 			if action == "play":
-				self.core.playback.play()
+				self.playbackPlay()
+			elif action == "playFirst":
+				tracks = self.tracklistSlice(0, 1)
+				track = tracks.get()[0]
+				self.playbackPlay(track)
 			elif action == "stop":
 				self.core.playback.stop()
 			elif action == "next":
@@ -67,29 +72,52 @@ class BotRockFrontend(pykka.ThreadingActor, core.CoreListener):
 				self.core.playback.pause()
 			elif action == "resume":
 				self.core.playback.resume()
-			elif action == "playTrack":
-				tl_track = data[u'tl_track']
-				tlid = data[u'tlid']
-				self.core.playback.play(tl_track, tlid)
 			elif action == "tracklistAdd":
 				uris = data[u'uris']
 				at_position = data[u'at_position']
-				tracks = self.core.tracklist.add(at_position = at_position, uris = uris)
-				return tracks
-			elif action == "playImmediate":
-				tracklistAddData = {}
-				tracklistAddData[u'at_position'] = 0
-				tracklistAddData[u'uris'] = data[u'uris']
-				tracks = self.handle_action(action = "tracklistAdd", data = tracklistAddData)
+				return self.tracklistAdd(uris, at_position)
+			elif action == "playNow":
+				insertIndex = 0
+				currentTrack = self.playbackGetCurrentTlTrack().get()
+				if currentTrack:
+					insertIndex = self.tracklistIndex(currentTrack).get()
+				#logger.info(currentTrack)
+				tracks = self.tracklistAdd(data[u'uris'], insertIndex)
 				track = tracks.get()[0]
-				logger.info(track)
-				playTrackData = {}
-				playTrackData[u'tl_track'] = track
-				playTrackData[u'tlid'] = None
-				self.handle_action(action = "playTrack", data = playTrackData)
+				self.playbackPlay(track)
 			else:
 				logger.info("Unhandled action")
 			pass
 		except Exception as e:
-			logger.info(e)
+			logger.error(e)
 		
+	def playbackPlay(self, tl_track = None, tlid = None):
+		return self.core.playback.play(tl_track, tlid)
+
+	def playbackStop(self):
+		return self.core.playback.stop()
+
+	def playbackNext(self):
+		return self.core.playback.next()
+
+	def playbackPrevious(self):
+		return self.core.playback.previous()
+
+	def playbackPause(self):
+		return self.core.playback.pause()
+
+	def playbackResume(self):
+		return self.core.playback.resume()
+
+	def playbackGetCurrentTlTrack(self):
+		return self.core.playback.get_current_tl_track()
+
+
+	def tracklistAdd(self, uris, at_position = 0):
+		return self.core.tracklist.add(at_position = at_position, uris = uris)
+
+	def tracklistSlice(self, start, end):
+		return self.core.tracklist.slice(start, end)
+
+	def tracklistIndex(self, tl_track = None, tlid = None):
+		return self.core.tracklist.index(tl_track, tlid)
